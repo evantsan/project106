d1b1b75dcc866eb2cc165e852589783fe280e748
evan <86027701+evantsan@users.noreply.github.com>
2022-12-03 10:12:31 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+function star()
+{
+    navigator.mediaDevices.getUserMedia({audio:true})
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/W4ePtHhth/model.json",modelLoaded)
+}
